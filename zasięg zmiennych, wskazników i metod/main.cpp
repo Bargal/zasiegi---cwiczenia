95a5b795053d0abe2ac43dadd1f29fab37c795cd
Bartosz Gala <bartosz.gala@mail.ru>
2017-12-03 13:04:17 +0100

    Ćwiiczenia z zasiegow - start.

---
+++ zasięg zmiennych, wskazników i metod/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "witam.\n w tym projekcie bêdê testowaæ zasiêgi zmiennych, klas i wska¿ników miêdzy funkcjami, klasami i plikami projektu\n" <<
+		"cwiczenia obejma te¿ dziedziczenie i zaprzyjaznianie klas" << std::endl;
+}
